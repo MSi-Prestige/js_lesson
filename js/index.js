58bf49e0c255e1fcf582f362723a3fb0b111ce38
IljaK <iljakristafenko@gmail.com>
2020-11-23 00:03:18 +0200

    new stpe 2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-let numberOfFilms = "Сколько фильмов ты посмотрел?";
+let numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
+
+let answerFilm = prompt("Один из любимых фильмов?", "");
+let answerStars = +prompt("На сколько оцениваете его?", "");
+let answerFilm2 = prompt("Один из любимых фильмов?", "");
+let answerStars2 = +prompt("На сколько оцениваете его?", "");
+
+
 const personalMovieDB = {
+
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -7,8 +15,10 @@ const personalMovieDB = {
     privat: false
 };
 
-let answerFilm = prompt("Один из любимых фильмов?");
-let answerStars = prompt("На сколько оцениваете его?");
+personalMovieDB.movies[answerFilm] = answerStars;         // ! chere [] < takie skobki - ne 4erez tochku
+personalMovieDB.movies[answerFilm2] = answerStars2;
+
+console.log(personalMovieDB);
 
 
 

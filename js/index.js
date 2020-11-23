f064f9c12607f4318ebbe7fd21df266230206f00
IljaK <iljakristafenko@gmail.com>
2020-11-23 22:10:30 +0200

    lesson2 MovieDB while-If

--- js/index.js
+++ js/index.js
@@ -1,24 +1,36 @@
+"use strict";
 let numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
 
-let answerFilm = prompt("Один из любимых фильмов?", "");
-let answerStars = +prompt("На сколько оцениваете его?", "");
-let answerFilm2 = prompt("Один из любимых фильмов?", "");
-let answerStars2 = +prompt("На сколько оцениваете его?", "");
-
-
-const personalMovieDB = {
-
+let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+let i = 0;
+while (i < 2) {
+    const a = prompt("Один из любимых фильмов?", ""),
+        b = prompt("На сколько оцениваете его?", "");
+    if (a != null && b != null && a != "" && b != "" && a.length < 50 && b.length < 2) {
+        personalMovieDB.movies[a] = b;
+        i++;
+        console.log("done");
+    }
+    else {
+        console.log("error");
+        i--;
+    }
+}
 
-personalMovieDB.movies[answerFilm] = answerStars;         // ! chere [] < takie skobki - ne 4erez tochku
-personalMovieDB.movies[answerFilm2] = answerStars2;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {         // ! count > 10 i count < 30
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("error");
+}
 
 console.log(personalMovieDB);
-
-
-
